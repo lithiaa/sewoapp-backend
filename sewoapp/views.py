3bf1a6b88372882984db7b77651b2cccabbbbd45
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-04-26 22:15:55 +0700

    feat: implement viewsets for User, Vehicle, Booking, Payment, and Review models

--- sewoapp/views.py
+++ sewoapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Vehicle, Booking, Payment, Review
+from .serializers import UserSerializer, VehicleSerializer, BookingSerializer, PaymentSerializer, ReviewSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
