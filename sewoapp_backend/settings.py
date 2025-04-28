53bcb75e893550e04813e3140cc467b753ad4d3b
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-04-28 20:42:55 +0700

    feat: configure static files

--- sewoapp_backend/settings.py
+++ sewoapp_backend/settings.py
@@ -130,7 +130,22 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#VM Only
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+}
+
+
