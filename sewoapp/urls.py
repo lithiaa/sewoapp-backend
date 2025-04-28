7b5534a81c5045d25484eda4b2849db9eb47f5b3
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-04-28 20:42:17 +0700

    refactor: adjust URL patterns

--- sewoapp/urls.py
+++ sewoapp/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, VehicleViewSet, BookingViewSet, PaymentViewSet, ReviewViewSet
+from .views import VehicleViewSet, BookingViewSet, PaymentViewSet, ReviewViewSet, QRCodeViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'vehicles', VehicleViewSet)
 router.register(r'bookings', BookingViewSet)
 router.register(r'payments', PaymentViewSet)
 router.register(r'reviews', ReviewViewSet)
+router.register(r'qrcodes', QRCodeViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
