457a0ea05653d6c4173feb93d5b7a4e7919c473d
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-04-29 11:44:52 +0700

    fix: remove created_at field from User model

--- sewoapp/models.py
+++ sewoapp/models.py
@@ -24,7 +24,6 @@ class User(AbstractUser):
     id_card_number = models.CharField(max_length=20, blank=True, null=True)
     id_card_photo = models.URLField(blank=True, null=True)
     is_verified = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
     
     REQUIRED_FIELDS = ['email', 'role']
 
