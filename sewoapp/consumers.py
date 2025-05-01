cc083b137343d21a1c13cc49f44542faeba0ff34
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2025-05-01 16:18:00 +0700

    feat: add ChatConsumer and routing for WebSocket chat functionality

---
+++ sewoapp/consumers.py
@@ -0,0 +1,50 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+from .models import Message, Conversation
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
+        self.room_group_name = f'chat_{self.conversation_id}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        sender_id = text_data_json['sender_id']
+
+        # Save message to database
+        conversation = await Conversation.objects.aget(id=self.conversation_id)
+        await Message.objects.acreate(
+            conversation=conversation,
+            sender_id=sender_id,
+            content=message
+        )
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'sender_id': sender_id
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message'],
+            'sender_id': event['sender_id']
+        }))
